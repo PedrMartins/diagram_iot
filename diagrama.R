5cba03a819434a5d3d532e5f19dae32e35d597d7
Pedro Martins <62296977+PedrMartins@users.noreply.github.com>
2024-10-02 13:22:24 -0300

    Add files via upload

--- diagrama.R
+++ diagrama.R
@@ -39,7 +39,7 @@ arrows(x0 = 2, y0 = 5.4, x1 = 12.5,
 rect(xleft=5, ybottom=4.5, xright=10, ytop=6.5, density = NULL, angle = 45,
      col = "lightblue", border = NULL, lty = par("lty"), lwd = par("lwd"))
 text("Object  \n detection", x=7.5, y = 5.5, cex= 1)
-text("Object \n detected", x=14.5, y = 5.3, cex= 0.9)
+text("Objects \n detected", x=13.5, y = 5.3, cex= 0.9)
 
 dev.off()
 
@@ -55,13 +55,16 @@ rect(xleft=0.5, ybottom=6.5, xright=3.5, ytop=8, density = NULL, angle = 45,
 text("Crop out \n 1.0", x=2, y = 7.3, cex= 0.8)
 rect(xleft=0.5, ybottom=3, xright=3.5, ytop=4.8, density = NULL, angle = 45,
      col = "lightblue", border = NULL, lty = par("lty"), lwd = par("lwd"))
-text("Image \n downsize \n of X times 1.2", x=1.9, y = 3.9, cex= 0.8)
+text("Image \n downsize \n of X times 1.2", 
+     x=1.9, y = 4, cex= 0.8)
 rect(xleft=5.5, ybottom=3, xright=8.5, ytop=4.8, density = NULL, angle = 45,
      col = "lightblue", border = NULL, lty = par("lty"), lwd = par("lwd"))
-text("Areas of Interest \n detection \n 1.3", x=6.5, y = 3.9, cex= 0.8)
+text("Areas of Interest \n detection \n 1.3", 
+     x=7, y = 3.9, cex= 0.8)
 rect(xleft=10.5, ybottom=3, xright=15.5, ytop=4.8, density = NULL, angle = 45,
      col = "lightblue", border = NULL, lty = par("lty"), lwd = par("lwd"))
-text("Select bounding boxes \n conficence trheshold of Y  \n 1.4", x=13.4, y = 3.9, cex= 0.8)
+text("Select bounding boxes \n conficence trheshold of Y  \n 1.4", 
+     x=13, y = 3.9, cex= 0.8)
 text("Areas of  Interest \n position", x=20.4, y = 3.9, cex= 0.8)
 
 dev.off()
@@ -72,8 +75,13 @@ plot(x = c(1,40), y = c(1,12), type="n", xlab = "",
 text("Areas of  Interest \n position", x=2, y = 9.5, cex= 0.9)
 arrows(x0 = 4.5, y0 = 9.5, x1 = 5.3, 
        col = "black", length = 0.1, angle = 45)
+arrows(x0 = 2.5, y0 = 8, y1 = 5.5, 
+       col = "black", length = 0.1, angle = 45,lty=2)
 rect(xleft=5.5, ybottom=7.5, xright=11.5, ytop=11.5, density = NULL, angle = 45,
      col = "lightblue", border = NULL, lty = par("lty"), lwd = par("lwd"))
+segments(10,3.5,y1=7.5, lty=2)
+arrows(x0 = 10, y0 = 3.5, x1 = 11.5, 
+       col = "black", length = 0.1, angle = 45, lty=2)
 text("Crop out the AoI \n from the original high \n resolution image \n 2.1",
      x=8.5, y = 9.5, cex= 0.9)
 arrows(x0 = 11.8, y0 = 9.5, x1 = 12.3, 
@@ -96,18 +104,22 @@ text("Select the \n Detected \n Bounding \n Boxes with \n Confidence \n Threshol
      x=27.9, y = 7.5, cex= 0.9)
 arrows(x0 = 30.8, y0 = 9.5, x1 = 35, 
        col = "black", length = 0.1, angle = 45)
+
 rect(xleft=1, ybottom=2, xright=8, ytop=5, density = NULL, angle = 45,
      col = "lightgreen", border = NULL, lty = 2, lwd = par("lwd"))
 text("Area of Interest \n Repositioning \n 2.0",
      x=4.5, y = 3.5, cex= 0.9)
-arrows(x0 = 30.8, y0 = 9.5, x1 = 35, 
+segments(8,3.5,x1=8.5, lty=2)
+arrows(x0 = 8.5, y0 = 3.5, y1 = 7.3, 
        col = "black", length = 0.1, angle = 45,lty=2)
 rect(xleft=12, ybottom=2, xright=20, ytop=5, density = NULL, angle = 45,
      col = "lightgreen", border = NULL, lty = 2, lwd = par("lwd"))
 text("Frame of Interest \n Generation \n 2.2a",
      x=16, y = 3.5, cex= 0.9)
+arrows(x0 = 14.5, y0 = 5, y1 = 7.3, 
+       col = "black", length = 0.1, angle = 45,lty=2)
 text("Objects \n detected",
      x=37, y = 9.5, cex= 0.9)
 
 
-(5 + 2)/2
+
